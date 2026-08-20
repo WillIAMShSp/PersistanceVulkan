8dfcb57db4714f47a03f47c7db2f74d10be7022d
Luis Camilo Alvarez Carrau <luiscamiloalvare@gmail.com>
2026-08-19 22:49:07 -0500

    Change: Added clarification to the shader compiling scripts

--- PersistanceVulkan/res/Shaders/compileBasicShader.bat
+++ PersistanceVulkan/res/Shaders/compileBasicShader.bat
@@ -1,3 +1,5 @@
-C:/VulkanSDK/1.4.321.1/Bin/glslc.exe BasicVertex.vert -o basicvert.spv
+
+# Change the directory to the glsc.exe that comes from the VulkanSDK
+C:/VulkanSDK/1.4.321.1/Bin/glslc.exe BasicVertex.vert -o basicvert.spv 
 C:/VulkanSDK/1.4.321.1/Bin/glslc.exe BasicFragment.frag -o basicfrag.spv
 pause
